062c4ddd9ca2ee772e57c0f00c23fb8f20112719
22R11A05U7 A K DINESH <22r11a05u7@gcet.edu.in>
2025-08-21 15:38:51 +0000

    other than api key is there any other problem??

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDA98mp868M8n-wJPvD-anR7hkIrgOopE8',
-  authDomain: 'echovault-1b80q.firebaseapp.com',
-  projectId: 'echovault-1b80q',
-  storageBucket: 'echovault-1b80q.appspot.com',
-  messagingSenderId: '361403673991',
-  appId: '1:361403673991:web:c4ecea2232da4af3b7f722',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
